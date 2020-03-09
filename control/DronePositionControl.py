7f708253e0f04e924b16a1415c521d72de404b54
Ratoone <vacariu.paul@gmail.com>
2020-03-09 21:26:10 +0100

    Drone control and basic logging

---
+++ control/DronePositionControl.py
@@ -0,0 +1,10 @@
+class DronePositionControl:
+    def __init__(self):
+        # TODO: control parameters and stuff
+        pass
+
+    def compute_drone_commands(self, drone_info):
+        # TODO: do the actual 3D control
+        return []
+
+    # TODO: add necessary control functions: PID, whatever
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import logging
+import signal
+import time
+
 from DroneInfo import DroneInfo
 from control.DroneControl import DroneControl
+from control.DronePositionControl import DronePositionControl
 from vision.ImageSegmentation import ImageSegmentation
 
 
@@ -7,12 +12,15 @@ class MainClass:
     def __init__(self):
         self.running = False
         self.drone = DroneControl()
+        self.drone_controller = DronePositionControl()
         self.image_segmentation = ImageSegmentation()
         self.main_window = None
-        self.time_sample = 0.05 # seconds
+        self.task_period = 0.05  # seconds
+        self.logger = logging.getLogger(self.__class__.__name__)
 
     def start(self):
         self.running = True
+        self.logger.info("Started System")
         self.run()
 
     def run(self):
@@ -21,8 +29,18 @@ class MainClass:
             info = self.image_segmentation.extract_information(image)
             # TODO: use info to complete drone_info
             drone_info = DroneInfo()
-            
+            control = self.drone_controller.compute_drone_commands(drone_info)
+            # TODO: something like self.drone.send_commands(control) - 3D control
+            # self.main_window.update(drone_info, image)
+            time.sleep(self.task_period)
+
+    def stop(self):
+        self.running = False
+        self.logger.info("Stopped System")
 
 
 if __name__ == "__main__":
-    MainClass().start()
+    logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - [%(name)s] %(levelname)s: %(message)s")
+    main = MainClass()
+    signal.signal(signal.SIGINT, lambda sig, frame: main.stop())
+    main.start()
